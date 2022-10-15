802c09b2fdfb1f7d84135cd2d9d17a2404ff915e
dangorogoro <dangorogoro0802@gmail.com>
2022-10-15 22:30:58 +0900

    Linting main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2021 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2021 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -69,15 +69,15 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
   static uint32_t callback_counter = 0;
   const uint32_t frequency = 100 * 10000;
-  if (htim == &htim6) //100kHz
+  if (htim == &htim6) // 100kHz
   {
     callback_counter = (callback_counter + 1) % frequency;
-    if(callback_counter == 0)
+    if (callback_counter == 0)
     {
-      //HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5);
+      // HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5);
     }
     ir_sampling();
-    if(callback_counter % 50 == 0)//2kHz
+    if (callback_counter % 50 == 0) // 2kHz
     {
       ir_update();
     }
@@ -86,13 +86,12 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -101,6 +100,9 @@ int main(void)
   HAL_Init();
 
   /* USER CODE BEGIN Init */
+  SEGGER_RTT_Init();
+  SEGGER_RTT_printf(1, "HELO WORLD\r\n");
+  SEGGER_RTT_printf(0, "HELO WORLD\r\n");
 
   /* USER CODE END Init */
 
@@ -125,25 +127,34 @@ int main(void)
   MX_TIM8_Init();
   /* USER CODE BEGIN 2 */
   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5, GPIO_PIN_SET);
-  while(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) == GPIO_PIN_RESET);
+  while (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) == GPIO_PIN_RESET)
+    ;
   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5, GPIO_PIN_RESET);
-  /*
+
   HAL_ADC_Start(&hadc2);
   if (HAL_ADC_Start_DMA(&hadc2, (uint32_t *)g_adc_data, 5) != HAL_OK)
   {
     Error_Handler();
   }
-  
+
+  /*
   HAL_TIM_Base_Start_IT(&htim6);
   ir_flash_start();
   */
   HAL_TIM_Base_Start_IT(&htim4);
   start_encoder();
 
-  
   SEGGER_RTT_printf(0, "GYRO WHO_AM_I %d\n", spi_gyro_read(GYRO_WHO_AM_I));
+  spi_gyro_read(GYRO_WHO_AM_I);
+  spi_gyro_read(GYRO_WHO_AM_I);
+  spi_gyro_read(GYRO_WHO_AM_I);
+  if (spi_gyro_read(GYRO_WHO_AM_I) != 215)
+  {
+    while (1)
+      ;
+  }
   set_gyro_configuration();
-  
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -153,7 +164,7 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-    //HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5);
+    // HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5);
 
     /*
     SEGGER_RTT_printf(0, "ADC %d %d %d %d\n", adc_data[0], adc_data[1], adc_data[2], adc_data[3]);
@@ -168,19 +179,17 @@ int main(void)
     set_motor_pulse(50,50);
     */
     SEGGER_RTT_printf(0, "TIM3: %d, TIM4: %d\n", TIM2->CNT, TIM8->CNT);
-    
-    /*
+
     SEGGER_RTT_printf(0, "GYRO %d\n", read_gyro_z());
-    SEGGER_RTT_printf(0, "ADC %d %d %d %d\n", 
-    ir_sensor_value[0], 
-    ir_sensor_value[1], 
-    ir_sensor_value[2], 
-    ir_sensor_value[3]);
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, ir_sensor_value[0] >= 2300);//Left front
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, ir_sensor_value[1] >= 2600);//Right front
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, ir_sensor_value[2] >= 2200);//Left
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, ir_sensor_value[3] >= 2200);//Right
-    */
+    SEGGER_RTT_printf(0, "ADC %d %d %d %d\n",
+                      ir_sensor_value[0],
+                      ir_sensor_value[1],
+                      ir_sensor_value[2],
+                      ir_sensor_value[3]);
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, ir_sensor_value[0] >= 2300); // Left front
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, ir_sensor_value[1] >= 2600); // Right front
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, ir_sensor_value[2] >= 2200); // Left
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, ir_sensor_value[3] >= 2200); // Right
 
     HAL_Delay(100);
   }
@@ -188,21 +197,21 @@ int main(void)
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Configure the main internal regulator output voltage
-  */
+   */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -217,9 +226,8 @@ void SystemClock_Config(void)
     Error_Handler();
   }
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
@@ -236,9 +244,9 @@ void SystemClock_Config(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -250,14 +258,14 @@ void Error_Handler(void)
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
@@ -266,4 +274,3 @@ void assert_failed(uint8_t *file, uint32_t line)
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
-
