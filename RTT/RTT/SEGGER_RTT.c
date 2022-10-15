1d00be9390a2acae3710eedb198ea7e2ce49a7d4
dangorogoro <dangorogoro0802@gmail.com>
2022-10-15 22:30:23 +0900

    Update the version of RTT

--- RTT/RTT/SEGGER_RTT.c
+++ RTT/RTT/SEGGER_RTT.c
@@ -3,7 +3,7 @@
 *                        The Embedded Experts                        *
 **********************************************************************
 *                                                                    *
-*            (c) 1995 - 2019 SEGGER Microcontroller GmbH             *
+*            (c) 1995 - 2021 SEGGER Microcontroller GmbH             *
 *                                                                    *
 *       www.segger.com     Support: support@segger.com               *
 *                                                                    *
@@ -41,6 +41,11 @@
 * DAMAGE.                                                            *
 *                                                                    *
 **********************************************************************
+*                                                                    *
+*       RTT version: 7.58d                                           *
+*                                                                    *
+**********************************************************************
+
 ---------------------------END-OF-HEADER------------------------------
 File    : SEGGER_RTT.c
 Purpose : Implementation of SEGGER real-time transfer (RTT) which
